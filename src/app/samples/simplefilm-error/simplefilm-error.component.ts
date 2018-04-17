4cf5f2ad551ca974d5ff70c5da9544f22b41c5a4
Sander Elias <sanderelias+git@gmail.com>
2018-04-18 00:44:28 +0200

    added error isolation sample

--- src/app/samples/simplefilm-error/simplefilm-error.component.ts
+++ src/app/samples/simplefilm-error/simplefilm-error.component.ts
@@ -1,7 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Movie } from '../sw-interfaces';
+import { Movie, RootMovies, RootObject } from '../sw-interfaces';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  concat,
+  concatMap,
+  tap,
+  catchError,
+  filter
+} from 'rxjs/operators';
+import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import { SwUrlService } from '../sw-url.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-simplefilm-error',
@@ -9,24 +22,42 @@ import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
   styles: []
 })
 export class SimplefilmErrorComponent implements OnInit {
-
   films: Movie[];
 
-  input = new FormControl('')
+  input = new FormControl('');
 
   update$ = this.input.valueChanges.pipe(
     debounceTime(500),
     distinctUntilChanged(),
-    map((key: string) => key.toLocaleLowerCase())
-  )
+    map((key: string) => +key),
+    filter(n => n > 0)
+  );
 
-  constructor() {}
+  filmsNoIsolation$ = this.update$.pipe(
+    concatMap<number, RootObject<Movie>>(id =>
+      this.http.get<RootObject<Movie>>(`http://swapi.co/api/swFilms/${id}`)
+    ),
+    map(result => result.results.title),
+    catchError(e => of(`Movie not found`))
+  );
+
+  filmsIsolation$ = this.update$.pipe(
+    concatMap<number, RootObject<Movie>>(id =>
+      this.http
+        .get<RootObject<Movie>>(`http://swapi.co/api/swFilms/${id}`)
+        .pipe(
+          map(result => result.results.title),
+          catchError(e => of(`Movie not found`))
+        )
+    )
+  );
+
+  constructor(private http: HttpClient, private swUrlService: SwUrlService) {}
 
   ngOnInit() {
     // const films$ = this.filmsService.getFilms();
+    console.log(this.swUrlService.url);
 
-    films$.subscribe(
-      data => (this.films = data.results)
-    );
+    // this.films$.subscribe(); //(data => (this.films = data.results));
   }
 }
