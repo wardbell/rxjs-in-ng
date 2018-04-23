d9e4acb89b8a8e075351b8d7529cdb602e5ea024
Sander Elias <sanderelias+git@gmail.com>
2018-04-23 08:24:28 +0200

    onoffbutton updates

--- projects/onoffbutton/src/lib/onoffbutton.component.ts
+++ projects/onoffbutton/src/lib/onoffbutton.component.ts
@@ -1,39 +1,15 @@
 // tslint:disable:member-ordering
-import {
-  Component,
-  OnInit,
-  forwardRef,
-  AfterViewInit,
-  Input,
-  ViewChild
-} from '@angular/core';
-import {
-  NG_VALUE_ACCESSOR,
-  ControlValueAccessor,
-  FormControl
-} from '@angular/forms';
+import { AfterViewInit, Component, HostListener, Input, OnInit, ViewChild, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'on-off-button',
   template: `
-	<input type="checkbox" [formControl]="toggleState">
 	<button #button><ng-content></ng-content></button> F
   `,
   styles: [
     `
-  :host input {
-    /* First, we make it as wide as the container */
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    /* Then, we put it on top of everything else */
-    z-index: 100;
-    /* Last, we make it invisible */
-    opacity: 0;
-    /* This one is just for ergonomy */
-    cursor: pointer;
-}
 
 button {
   border:1px solid;
@@ -55,19 +31,25 @@ button {
 })
 export class OnOffButtonComponent
   implements OnInit, AfterViewInit, ControlValueAccessor {
-  private _val;
+  private _val = false;
   onChange = x => x;
   onTouched = x => x;
   disabled = false;
   @Input()
-  set checked(newVal) {
-    this.toggleState.setValue(newVal != 'false');
+  set checked(newVal: any) {
+    // tslint:disable-next-line:triple-equals
+    this._val = newVal != 'false';
     this.update();
   }
+  get checked() { return this._val; }
 
   @ViewChild('button') buttonRef;
   private button: HTMLButtonElement;
-  toggleState = new FormControl({ value: false, disabled: this.disabled });
+
+  @HostListener('click')
+  onclick() {
+    this.update(!this._val);
+  }
 
   constructor() {
     this.update = this.update.bind(this);
@@ -88,13 +70,13 @@ export class OnOffButtonComponent
 
   setDisabledState(isDisabled: boolean): void {
     this.disabled = isDisabled;
-    this.toggleState[isDisabled ? 'disable' : 'enable']();
+    // this.toggleState[isDisabled ? 'disable' : 'enable']();
   }
 
   ngOnInit() {}
 
-  update(newVal = this.toggleState.value) {
-    this.onChange(newVal ? this._val : undefined);
+  update(newVal = this._val) {
+    this.onChange(newVal);
     if (this.button) {
       this.button.style.backgroundColor = newVal ? 'blue' : 'gray';
     }
@@ -102,7 +84,7 @@ export class OnOffButtonComponent
 
   ngAfterViewInit() {
     this.button = this.buttonRef.nativeElement;
-    this.toggleState.valueChanges.subscribe(this.update);
+    // this.toggleState.valueChanges.subscribe(this.update);
     setTimeout(this.update, 0);
   }
 }
--- projects/onoffbutton/src/public_api.ts
+++ projects/onoffbutton/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of onoffbutton
  */
 
-export * from './lib/onoffbutton.service';
 export * from './lib/onoffbutton.component';
 export * from './lib/onoffbutton.module';
